decdab7d4276cd6fe0ebdc9e2a164771293c0e6b
Roberto Ribeiro <robertov82008@gmail.com>
2020-02-29 20:00:59 -0300

    04 - Estilos globais

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['react', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': [
